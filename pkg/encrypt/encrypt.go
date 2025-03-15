7d99419b012569cc4be09f9dc2717711bb5cfd24
d2bz <3180324210@qq.com>
2025-03-15 23:14:07 +0800

    pref: 信息加密

--- pkg/encrypt/encrypt.go
+++ pkg/encrypt/encrypt.go
@@ -1,1 +1,72 @@
 package encrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var mobileKey = []byte("this-is-a-dev-key-32-bytes-long!")
+
+// 密码永远不用明文存储，单向哈希即可
+func EnPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+
+	}
+	return string(hashedPassword), nil
+}
+
+func CheckPassword(password, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
+
+func EnMobile(mobile string) (string, error) {
+	block, err := aes.NewCipher(mobileKey)
+	if err != nil {
+		return "", err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+	cipherText := gcm.Seal(nil, nonce, []byte(mobile), nil)
+	combined := append(nonce, cipherText...)
+	return base64.StdEncoding.EncodeToString(combined), nil
+}
+
+func DeMobile(encryptedMobile string) (string, error) {
+	combined, err := base64.StdEncoding.DecodeString(encryptedMobile)
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher(mobileKey)
+	if err != nil {
+		return "", err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	nonceSize := gcm.NonceSize()
+	if len(combined) < nonceSize {
+		return "", errors.New("invalid encrypted data")
+	}
+	nonce, ciphertext := combined[:nonceSize], combined[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+	return string(plaintext), nil
+}
